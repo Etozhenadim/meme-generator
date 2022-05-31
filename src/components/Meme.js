e4f1ac3c65352fdd1b2f8cf2b0002ba8ba6bb16e
n.iskakov <n.iskakov@platform.live>
2022-05-31 15:03:30 +0200

    #3 memes generator

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,14 +1,24 @@
 import React from "react";
+import memesData from "../memesData.js"
 
 
 export default function Meme(){
+
+    // let url =
+
+    function getMemeImage(){
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        alert(randomNumber)
+        // {url} = memesArray[randomNumber].url
+    }
 
     return(
        <main>
            <form action="" className="form" >
                <input type="text" className="form--input" placeholder="top"/>
                <input type="text" className="form--input" placeholder="bottom"/>
-               <button className="form--button">Get a new meme image 🖼</button>
+               <button onClick={getMemeImage} className="form--button">Get a new meme image 🖼</button>
            </form>
        </main>
     )
